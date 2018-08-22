a5972fac1881fca22cf6c0a5ed894ac2f20d1e82
Raphaël DUCOM <raphael.ducom@gmail.com>
2018-08-22 14:48:11 +0200

    missing proto

--- Application/RDD.Application/IAppController.cs
+++ Application/RDD.Application/IAppController.cs
@@ -10,6 +10,7 @@ namespace RDD.Application
         where TEntity : class, IEntityBase<TEntity, TKey>
         where TKey : IEquatable<TKey>
     {
+        Task<TEntity> CreateAsync(TEntity candidateEntity);
         Task<TEntity> CreateAsync(ICandidate<TEntity, TKey> candidate, Query<TEntity> query);
         Task<TEntity> UpdateByIdAsync(TKey id, ICandidate<TEntity, TKey> candidate, Query<TEntity> query);
         Task<IEnumerable<TEntity>> UpdateByIdsAsync(IDictionary<TKey, ICandidate<TEntity, TKey>> candidatesByIds, Query<TEntity> query);
